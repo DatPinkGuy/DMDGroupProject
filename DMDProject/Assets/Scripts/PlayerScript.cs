1abed8c8d9b12a88d98de6aa8d8e4f1ab0a3b5cb
Alex <ovcinikov@gmail.com>
2019-10-11 13:44:37 +0100

    NavMesh map movement
    Added prototype of map, player moves to nodes

---
+++ DMDProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerScript : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50))
+            {
+                if (hit.transform.CompareTag("Node"))
+                {
+                    agent.destination = hit.transform.position;
+                }
+            }
+        }
+    }
+}
