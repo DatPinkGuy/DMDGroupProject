7503ce817297ff824ab28724965ba57b78659fed
Alex <ovcinikov@gmail.com>
2019-10-25 13:45:04 +0100

    Changes to Game Manager

--- DMDProject/Assets/Scripts/GameManager.cs
+++ DMDProject/Assets/Scripts/GameManager.cs
@@ -6,16 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    enum State
+    {
+        MainMap,
+        Biome
+    }
+
     private RaycastHit _hit;
     private LoadLevelInfo _levelInformation;
-    public Sprite outfit;
-    [SerializeField] private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!outfit) return;
-        player.GetComponent<SpriteRenderer>().sprite = outfit;
+        
     }
 
     // Update is called once per frame
@@ -25,13 +28,21 @@ public class GameManager : MonoBehaviour
         {
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _hit, 50))
             {
-                if (_hit.transform.GetComponent(typeof(LoadLevelInfo)))
+                if (_hit.transform.GetComponent<LoadLevelInfo>())
                 {
                     _levelInformation = _hit.transform.GetComponent<LoadLevelInfo>();
                     SceneManager.LoadScene(_levelInformation.levelNumb);
                 }
             }
         }
+    }
 
+    void CheckForNode()
+    {
+        if (_hit.transform.GetComponent<LoadLevelInfo>())
+        {
+            _levelInformation = _hit.transform.GetComponent<LoadLevelInfo>();
+            SceneManager.LoadScene(_levelInformation.levelNumb);
+        }
     }
 }
