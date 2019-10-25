8fa6adf7f6bbc6e92371c75c46de1fb38a4e84b0
Aleksandr <ovcinikov@gmail.com>
2019-10-25 22:09:09 +0100

    Changes to Loading code
    Changes to code for better future merging and productivity

--- DMDProject/Assets/Scripts/GameManager.cs
+++ DMDProject/Assets/Scripts/GameManager.cs
@@ -8,8 +8,11 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private RaycastHit _hit;
-    private LoadLevelInfo _levelInformation;
     [SerializeField] private List<GameObject> nodes;
+    [SerializeField] private new Camera camera;
+    private GameObject _chosenNode;
+    private LoadLevelInfo LevelInfo => _chosenNode.GetComponent<LoadLevelInfo>();
+    
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +25,15 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _hit, 50))
+            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out _hit, 50))
             {
-                if (_hit.transform.GetComponent<LoadLevelInfo>())
+                foreach (var node in nodes)
                 {
-                    _levelInformation = _hit.transform.GetComponent<LoadLevelInfo>();
-                    SceneManager.LoadScene(_levelInformation.levelNumb);
+                    if (_hit.transform == node.transform)
+                    {
+                        _chosenNode = node;
+                        SceneManager.LoadScene(LevelInfo.levelNumb);
+                    }
                 }
             }
         }
