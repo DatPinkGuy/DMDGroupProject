7f523861527fa05668bbca1ff4ff584be0fcb53c
dave <dnewman.1912@gmail.com>
2020-02-07 10:39:23 +0000

    added title screen and ui for main menu

---
+++ DMDProject/Assets/Scripts/sceneSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSwitcher : MonoBehaviour
+{
+
+    public void sceneSwitch(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+
+
+
+
+}
