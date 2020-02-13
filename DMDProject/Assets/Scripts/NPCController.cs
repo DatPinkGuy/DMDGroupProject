9a6c7134d74079cc0885ef16815d6271702b4448
Jin <Jinyaden@hotmail.com>
2020-02-13 15:35:46 +0000

    Deleted Biome Manager
    Since biomes are gonna be copy pasta no point in having one scene handle the asset switching :x

--- DMDProject/Assets/Scripts/NPCController.cs
+++ DMDProject/Assets/Scripts/NPCController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class NPCController : MonoBehaviour
 {
+    [SerializeField] float npcMoveSpeed;
+
+    private void Start()
+    {
+      
+    }
     private void Update()
     {
         NPCMovement();
@@ -12,6 +18,19 @@ public class NPCController : MonoBehaviour
     void NPCMovement()
     {
         //TODO: squash and stretch the npc
-        
+        GetComponent<Rigidbody2D>().velocity = new Vector2(npcMoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(CollosionDelay());
+    }
+
+    IEnumerator CollosionDelay()
+    {
+        yield return new WaitForSeconds(Random.Range(1, 3));
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        npcMoveSpeed *= -1;
+        StopCoroutine(CollosionDelay());
     }
 }
