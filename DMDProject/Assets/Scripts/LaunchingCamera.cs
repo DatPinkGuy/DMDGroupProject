5236a1753b6e3569d605dc300e287c7e42e8d8fd
Alex <ovcinikov@gmail.com>
2020-02-14 12:29:28 +0000

    Added a properly working power bar
    Added a properly working power bar that had various speed and goes both up and down

--- DMDProject/Assets/Scripts/LaunchingCamera.cs
+++ DMDProject/Assets/Scripts/LaunchingCamera.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LaunchingCamera : MonoBehaviour
 {
@@ -18,12 +19,17 @@ public class LaunchingCamera : MonoBehaviour
     private bool _playState;
     private Ray _ray;
     private RaycastHit _hit;
+    //
+    private bool _powerBarTop;
+    private bool _powerBarBottom;
+    //
     [SerializeField] private GameObject animalLaunching;
     [SerializeField] private new Camera camera;
+    [SerializeField] private Image powerBarImage;
     // Start is called before the first frame update
     void Start()
     {
-
+        powerBarImage.enabled = false;
     }
 
     // Update is called once per frame
@@ -41,6 +47,7 @@ public class LaunchingCamera : MonoBehaviour
     {
         Rb.AddForce(power*forceModifier*_launchDirection);
         _launched = true;
+        powerBarImage.enabled = false;
     }
 
     private Vector3 GetDirection(Vector3 startPoint, Vector3 endPoint)
@@ -48,6 +55,8 @@ public class LaunchingCamera : MonoBehaviour
         Vector3 launchDirection = endPoint + startPoint;
         launchDirection.z = animalLaunching.transform.position.z;
         _gotDirection = true;
+        powerBarImage.enabled = true;
+        powerBarImage.fillAmount = 0;
         return launchDirection;
     }
 
@@ -87,7 +96,36 @@ public class LaunchingCamera : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Mouse0)) _launchPower = 0;
             if (Input.GetKey(KeyCode.Mouse0))
             {
-                 if (_launchPower < maxPower) _launchPower += powerMeterSpeed * Time.deltaTime; //add bar lowering
+                if (!_powerBarTop)
+                {
+                    if (_launchPower < maxPower)
+                    {
+                        _launchPower += powerMeterSpeed * Time.deltaTime; //add bar lowering
+                        powerMeterSpeed += Time.deltaTime;
+                        powerBarImage.fillAmount = _launchPower / maxPower;
+                    }
+                    else
+                    {
+                        _powerBarTop = true;
+                        _powerBarBottom = false;
+                    }
+                }
+                else if (!_powerBarBottom)
+                {
+                    if (_launchPower > 0)
+                    {
+                        _launchPower -= powerMeterSpeed * Time.deltaTime;
+                        powerMeterSpeed -= Time.deltaTime;
+                        powerBarImage.fillAmount = _launchPower / maxPower;
+                    }
+                    else
+                    {
+                        _powerBarBottom = true;
+                        _powerBarTop = false;
+                    }
+                }
+               
+                
             }
             if (Input.GetKeyUp(KeyCode.Mouse0)) LaunchObject(_launchPower);
         }
