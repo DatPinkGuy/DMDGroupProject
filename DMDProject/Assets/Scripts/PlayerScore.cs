f6452a962ecba668deea46ae618eda5f2734fe15
Alex <ovcinikov@gmail.com>
2020-03-03 12:02:06 +0000

    Added spawning collectables in launching game

---
+++ DMDProject/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    private int _score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.GetComponent<CollectablePoints>()) return;
+        _score += other.GetComponent<CollectablePoints>().scoreAmount;
+        scoreText.text = _score.ToString();
+    }
+}
