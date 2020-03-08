0d3afaa5d71c0ceec3388337382aba3c8251f703
Jin <Jinyaden@hotmail.com>
2020-03-08 18:25:43 +0000

    Added Fishing Minigame
    Needs Sprite assets and a way to get back to Main Menu

---
+++ DMDProject/Assets/Scripts/FishAI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAI : MonoBehaviour
+{
+    float moveSpeed = 3f;
+    float rotSpeed = 100f;
+
+    bool isWandering = false;
+    bool isRotatingLeft = false;
+    bool isRotatingRight = false;
+    bool isWalking = false;
+
+    private void Update()
+    {
+        if (isWandering == false)
+        {
+            StartCoroutine(Wander());
+        }
+        if(isRotatingRight == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
+        }
+        if (isRotatingLeft == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
+        }
+        if(isWalking == true)
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+    IEnumerator Wander()
+    {
+        int rotTime = Random.Range(2, 3);
+        int rotateWait = Random.Range(1, 4);
+        int rotateLorR = Random.Range(1, 2);
+        int walkWait = Random.Range(1, 4);
+        int walkTime = Random.Range(1, 5);
+
+        isWandering = true;
+
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+        yield return new WaitForSeconds(rotateWait);
+        if(rotateLorR == 1)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+        }
+
+        if (rotateLorR == 2)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
+        }
+        isWandering = false;
+    }
+}
---
+++ DMDProject/Assets/Scripts/FishGameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FishGameManager : MonoBehaviour
+{
+    [SerializeField] GameObject catchButton, startButton, caughtText, winLossPanel;
+    [SerializeField] TextMeshProUGUI caughtNum, winPanelText;
+    [SerializeField] float fishingTimer = 0f;
+    [SerializeField] float catchTimer = 0f;
+    float caughtAmount;
+    bool canbeCaught = false;
+    bool startFish = false;
+    float CatchstopTime;
+    bool timerCanStart = false;
+
+    private void Update()
+    {
+        if (startFish)
+        {
+            caughtText.SetActive(false);
+            fishingTimer += Time.deltaTime;
+            float stopTime = Random.Range(2f, 10f);
+            if (fishingTimer >= stopTime)
+            {
+                // fish is on the reel
+                startFish = false;
+                catchButton.SetActive(true);
+                timerCanStart = true;
+                fishingTimer = 0f;
+            }
+        }
+
+        if (timerCanStart)
+        {
+            catchTimer += Time.deltaTime;
+            CatchstopTime = Random.Range(2f, 4f);
+        }
+        if (canbeCaught)
+        {
+            startButton.SetActive(true);
+            catchButton.SetActive(false);
+            caughtText.SetActive(true);
+            caughtText.GetComponent<TextMeshProUGUI>().text = "Caught some Junk!";
+            caughtAmount++;
+            caughtNum.text = caughtAmount.ToString();
+            canbeCaught = false;
+            timerCanStart = false;
+            catchTimer = 0f;
+        }
+
+        if (catchTimer >= CatchstopTime)
+        {
+            startButton.SetActive(true);
+            catchButton.SetActive(false);
+            caughtText.GetComponent<TextMeshProUGUI>().text = "Debris Got Away!";
+            catchTimer = 0f;
+            timerCanStart = false;
+        }
+
+
+        if (caughtAmount > 5)
+        {
+            winLossPanel.SetActive(true);
+            winPanelText.text = "You Win!";
+        }
+    }
+
+    public void StartFishing()
+    {
+        startButton.SetActive(false);
+        startFish = true;
+    }
+
+    public void CatchFish()
+    {
+        canbeCaught = true;
+    }
+
+    public void RestartFishGame()
+    {
+        caughtAmount = 0f;
+        catchButton.SetActive(false);
+        startButton.SetActive(true);
+        winLossPanel.SetActive(false);
+    }
+}
