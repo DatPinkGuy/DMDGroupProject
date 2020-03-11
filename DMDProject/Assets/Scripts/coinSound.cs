4edec920971f656f6f4b545cf8ad117f9ec07681
Gemma <gemm4885@hotmail.com>
2020-03-11 14:13:07 +0000

    Added Sound to Mini-Game

---
+++ DMDProject/Assets/Scripts/coinSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinSound : MonoBehaviour
+{
+    public AudioClip coin;
+    AudioSource audio;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "coin")
+        {
+            audio.clip = coin;
+            audio.Play();
+        }
+    }
+}
